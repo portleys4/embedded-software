e79595a6266eb9aaf666fa0ad8c89181348f014f
yuens7 <yuens7@students.rowan.edu>
2018-04-10 18:55:45 -0400

    added MQTT_Init function

---
+++ examples/STM32L475/mqtt.c
@@ -0,0 +1,53 @@
+#include "mqtt.h"
+
+
+int MQTT_Init(){
+	const char *serverAddress = NULL;
+	const char *pCaCert;
+	const char *pClientCert;
+	const char *pClientPrivateKey;
+	static IoT_Error_t rc = FAILURE;
+	static IoT_Client_Init_Params mqttInitParams;
+
+	AWS_IoT_Client client;
+	memset(&client, 0, sizeof(AWS_IoT_Client));
+	IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
+	
+	getServerAddress(&serverAddress);
+	getTLSKeys(&pCaCert, &pClientCert, &pClientPrivateKey);
+	
+	mqttInitParams.enableAutoReconnect = false; 
+	mqttInitParams.pHostURL = (char *) serverAddress;
+	mqttInitParams.port = AWS_IOT_MQTT_PORT;
+	mqttInitParams.pRootCALocation = (char *) pCaCert;
+	mqttInitParams.pDeviceCertLocation = (char *) pClientCert;
+	mqttInitParams.pDevicePrivateKeyLocation = (char *) pClientPrivateKey;
+	mqttInitParams.mqttCommandTimeout_ms = 20000;
+	mqttInitParams.tlsHandshakeTimeout_ms = 5000;
+	mqttInitParams.isSSLHostnameVerify = true;
+	mqttInitParams.disconnectHandler = disconnectCallbackHandler;
+	mqttInitParams.disconnectHandlerData = NULL;
+	rc = aws_iot_mqtt_init(&client, &mqttInitParams);
+
+	if(rc != 0){
+		msg_error("aws_iot_mqtt_init error");
+		return -1;
+	}	
+	return 0;
+}
+
+MQTT_Subscribe(char* topic, QOS qos, void (*callback)(char* data), char* pData){
+	
+}
+
+MQTT_Publish(char* topic, QOS qos, char* pData, uint8_t len){
+	
+}
+
+MQTT_Unsubscribe(char* topic){
+	
+}
+
+MQTT_StatusHandler(void (*callback)(int)){
+	
+}
