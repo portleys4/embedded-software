7ecc307852b2fd92f8b5db7285b9c83e4832ecbd
Steven Portley <portleys4@students.rowan.edu>
2018-04-10 20:50:34 -0400

    Added return values for function declarations

--- examples/STM32L475/mqtt.h
+++ examples/STM32L475/mqtt.h
@@ -3,12 +3,13 @@
 
 #include <stdint.h>
 #include "aws_iot_mqtt_client.h"
+#include "aws_iot_error.h"
 
 
 /**
 * Initalize the MQTT Device
 */
-MQTT_Init();
+Iot_Error_t MQTT_Init();
 
 /**
 * Subscribe to a topic
@@ -16,9 +17,10 @@ MQTT_Init();
 * @param topic - Topic to subscribe to
 * @param qos - Quality of Service of the MQTT connection
 * @param callback - Handles receiving data from the broker 
-* @param pData - 
+* @param data - Any data external data that the 
+* @param pData - Pointer to data that gets sent to the callback handler
 */
-MQTT_Subscribe(char* topic, QOS qos, void (*callback)(char* data), char* pData;
+Iot_Error_t MQTT_Subscribe(char* topic, QOS qos, void (*callback)(char* data, char*message), char* pData;
 
 /**
 * Publish to a topic
@@ -28,19 +30,19 @@ MQTT_Subscribe(char* topic, QOS qos, void (*callback)(char* data), char* pData;
 * @param pData - Data being published
 * @param len - Length of the data
 */
-MQTT_Publish(char* topic, QOS qos, char* pData, uint8_t len);
+Iot_Error_t MQTT_Publish(char* topic, QOS qos, char* pData, uint8_t len);
 
 /**
 * Unsubscribe from a topic
 * 
 * @param topic - The topic to unsubscribe from
 */
-MQTT_Unsubscribe(char* topic);
+Iot_Error_t MQTT_Unsubscribe(char* topic);
 
 /**
 * callback - Callback handler for when the connection is interrupted
 */
-MQTT_StatusHandler(void (*callback)(int));
+Iot_Error_t MQTT_StatusHandler(void (*callback)(int));
 
 
 #endif
