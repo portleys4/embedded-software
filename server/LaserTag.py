d7bddc5d309bb11a3aa502cb738741e7174a7582
steven <portleys4@students.rowan.edu>
2018-05-03 04:11:41 -0400

    now properly publishes start message, tracks score, tracks health & lives

--- server/LaserTag.py
+++ server/LaserTag.py
@@ -23,9 +23,9 @@ RULES = {
 
 aws_client = AWS("server", 
 "a213qaaw8pshxz.iot.us-east-1.amazonaws.com", 
-"/home/steve/Desktop/AWS_Certs/RootCA",
-"/home/steve/Desktop/AWS_Certs/fc21dd6d85-private.pem.key",
-"/home/steve/Desktop/AWS_Certs/fc21dd6d85-certificate.pem.crt")
+"/home/steven/Desktop/AWS_Certs/RootCA",
+"/home/steven/Desktop/AWS_Certs/fc21dd6d85-private.pem.key",
+"/home/steven/Desktop/AWS_Certs/fc21dd6d85-certificate.pem.crt")
 
 Game_Players = Players()
 
@@ -42,10 +42,8 @@ def register_player(client, userdata, message):
     if "preferred_team" in jsonMessage:
         preferred_team = jsonMessage["preferred_team"].encode("ascii")
         Game_Players.add_player(player_name, preferred_team)
-        print(player_name +  " has joined the game on team " + preferred_team)
     else:
         Game_Players.add_player(player_name)
-        print(player_name +  " has joined the game on team 'Default'")
 
 def unregister_player(client, userdata, message):
     message.payload = json_clean(message.payload)
@@ -59,8 +57,8 @@ def player_shoot(client, userdata, message):
     """
     message.payload = json_clean(message.payload)
     player_name = message.topic.encode("ascii")
-    player_name = player_name[28:]
-    player_name = player_name[:-6]
+    player_name = player_name[12:]
+    player_name = player_name[:-15]
     shots_fired = json.loads(message.payload)["shots_fired"]
     shots_fired = int(shots_fired)
     Game_Players.add_shots(player_name, shots_fired)
@@ -70,23 +68,27 @@ def player_shoot(client, userdata, message):
 def player_hit(client, userdata, message):
     message.payload = json_clean(message.payload)
     jsonMessage = json.loads(message.payload)
+    print("Topic name pre: " + message.topic.encode("ascii"))
     victim_name = message.topic.encode("ascii")
-    victim_name = victim_name[28:]
-    victim_name = victim_name[:-6]
+    victim_name = victim_name[12:]
+    victim_name = victim_name[:-13]
+    print("Victim name: " + victim_name)
     shooter_id = int(jsonMessage["id"])
     shooter_name = Game_Players.player_hit(victim_name, shooter_id, 20)
     victim_id = Game_Players.get_playerid(victim_name)
     if shooter_name:
         topic = "$aws/things/server/" + shooter_name + "/gottem"
         aws_client.AWS_Publish(topic, {"victim_ID" : victim_id})
-        print shooter_name + " has hit " + victim_name
+        print(shooter_name + " has hit " + victim_name)
     else:
-        print "Friendly fire between" + shooter_name + " and " + victim_name
+        print("Friendly fire between" + shooter_name + " and " + victim_name)
+
 
 aws_client.AWS_Subscribe("$aws/things/server/lasertag/register", register_player)
 aws_client.AWS_Subscribe("$aws/things/server/lasertag/unregister", unregister_player)
 
-raw_input("Type something to start the game!")
+raw_input("Type something to start the game!\n")
+
 
 aws_client.AWS_Unsubscribe("$aws/things/server/lasertag/register")
 aws_client.AWS_Unsubscribe("$aws/things/server/lasertag/unregister")
@@ -100,14 +102,19 @@ Subscribe to topics for all players in game
 Using wildcards doesn't seem to work?
 """
 for player in Game_Players.get_players():
-    aws_client.AWS_Subscribe("$aws/things/" + player.get_name() + "lasertag/shoot", player_shoot)
-    aws_client.AWS_Subscribe("$aws/things/" + player.get_name() + "lasertag/hit", player_hit)
+    shoot_topic = "$aws/things/" + player.get_name() + "/lasertag/shoot"
+    hit_topic = "$aws/things/" + player.get_name() + "/lasertag/hit"
+    print("Subscribing to " + shoot_topic)
+    print("Subscribing to " + hit_topic)
+    aws_client.AWS_Subscribe(shoot_topic, player_shoot)
+    aws_client.AWS_Subscribe(hit_topic, player_hit)
 
 
 print("Starting game!")
 aws_start_topic = "$aws/things/server/lasertag/start"
-start_message = RULES #Fix this
-print "Publishing to " + aws_start_topic " to begin the game"
-aws_client.AWS_Publish(aws_start_topic, start_message)
+aws_start_message = { "rules" : RULES, "team1" : Game_Players.team1, "team2" : Game_Players.team2}
+print("Publishing " + str(aws_start_message) + " to " + aws_start_topic + " to begin the game")
+aws_client.AWS_Publish(aws_start_topic, aws_start_message)
+
 while True:
     time.sleep(5)
--- server/Player.py
+++ server/Player.py
@@ -15,7 +15,7 @@ class Player(object):
         self.score += points
 
     def damage(self, damage):
-        if damage > self.health:
+        if damage >= self.health:
             if self.lives <= 1:
                 self.lives = 0#Dead
                 return True
@@ -28,25 +28,33 @@ class Player(object):
         else:
             self.health -= damage
         return False
+
     def get_name(self):
         return self.name
 
     def add_shots(self, shots):
         self.shots += shots
 
+    def get_playerid(self):
+        return self.player_id
+
 class Players(object):
     def __init__(self):
         self.players = {}
+        self.team1 = []
+        self.team2 = []
         self.player_counter = 0
 
     def player_hit(self, victim_name, shooter_id, damage):
         victim = self.players[victim_name]
-        for player in self.players:
+        for player in self.players.values():
             if player.player_id == shooter_id:
                 shooter = player
+                break
         else:
             return -1 
         if victim.team_name == shooter.team_name:
+            print("Friendly fire between " + victim_name + " and " + shooter.name)
             return 0 #Friendly fire
         if self.damage_player(victim_name, damage):
             self.add_score(shooter.name, 10)
@@ -54,23 +62,44 @@ class Players(object):
             self.add_score(shooter.name, 1)
         return shooter.name
 
-    def add_player(self, player_name, preferred_team = "Default"):
-        self.players[player_name] = Player(player_name, self.player_counter, preferred_team)
+    def add_player(self, player_name, preferred_team = None):
+        print(preferred_team)
+        if preferred_team == "team1":
+            self.players[player_name] = Player(player_name, self.player_counter, "team1")
+            self.team1.append({"name" : player_name, "id" : self.player_counter})
+            print(player_name + " has join the game on team " + "team1")
+        elif preferred_team == "team2":
+            self.team2.append({"name" : player_name, "id" : self.player_counter})
+            self.players[player_name] = Player(player_name, self.player_counter, "team2")
+            print(player_name + " has join the game on team " + "team2")
+        else:
+            if len(self.team1) <= len(self.team2):
+                self.players[player_name] = Player(player_name, self.player_counter, "team1")
+                self.team1.append({"name" : player_name, "id" : self.player_counter})
+                print(player_name + " has join the game on team 1")
+            else:
+                self.players[player_name] = Player(player_name, self.player_counter, "team2")
+                self.team2.append({"name" : player_name, "id" : self.player_counter})
+                print(player_name + " has join the game on team 2")
         self.player_counter += 1
 
     def remove_player(self, player_name):
         del self.players[player_name]
 
     def damage_player(self, player_name, damage):
+        print(str(damage) + " damage dealt to " + player_name)
         if self.players[player_name].damage(damage):
+            print(player_name + " has been killed")
             del self.players[player_name]
             return True
         return False
 
     def add_score(self, player_name, score):
+        print(str(score) + " added to " + player_name + "'s score")
         self.players[player_name].add_score(score)
 
     def add_shots(self, player_name, shots):
+        print(shots + " added to " + player_name + "'s shots")
         self.players[player_name].add_shots(shots)
 
     def get_players(self):
